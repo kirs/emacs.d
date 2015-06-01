e4f2b1337cd2f333f3116b897af85214ac0e976f
Kir Shatrov <shatrov@me.com>
2015-06-01 15:12:28 +0300

    el-get and textmate

--- init.el
+++ init.el
@@ -158,8 +158,21 @@
                                         ;(flymake-mode 1)
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+;;(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
 ;;(add-to-list 'load-path "~/.emacs.d/el-get/textmate/textmate.el")
 
-;;(require 'textmate)
-;;(textmate-mode)
+(require 'textmate)
+(textmate-mode)
 ;;(flyspell-)
